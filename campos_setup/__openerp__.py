cb74efcd8d7f0208da3ca10cad4f210f37275f55
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2015-03-20 16:23:19 +0100

    Initial CampOS standard setup. To be expanded when we add more modules.

---
+++ campos_setup/__openerp__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    CampOS Setup
+#    Copyright (C) 2015 Hans Henrik Gabelgaard
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': "CampOS Standard Setup",
+
+    'summary': """
+        Our Standard Odoo Config""",
+
+    'description': """
+        Loads Danish as secondary languages
+        Enables "Technical features" for Administrator and sets the timezone to Europe/Copenhagen
+        Removes the Odoo online bindings
+         
+        Requires the following OCA repos:
+            server-tools
+            
+            
+            
+    """,
+
+    'author': "Hans Henrik Gabelgaard",
+    'website': "dk.linkedin.com/in/hanshenrikgabelgaard",
+
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',
+                'disable_openerp_online',
+                'admin_technical_features',
+                ],
+
+    # always loaded
+    'data': [
+        'lang.xml',
+    ],
+    'application': True,
+}
