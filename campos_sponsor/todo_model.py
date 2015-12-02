aed97fba5db5aa1c4e04629451d66d5a024e6f25
patbassing <patrick.bassing@gmail.com>
2015-12-02 21:38:12 +0100

    Test

--- campos_sponsor/todo_model.py
+++ campos_sponsor/todo_model.py
@@ -2,7 +2,5 @@
 from openerp import models, fields
 class TodoTask(models.Model):
 	_name = 'model.sponsor'
-	name = fields.Char('Description', required=True)
-	is_done = fields.Boolean('Done?')
-	active = fields.Boolean('Active?', default=True)
-	partner_id = fields.Many2one('res.partner','Contact')
+	name = fields.Char('Navn', required=True)
+	partner_id = fields.Many2one('res.partner','SL2017 kontaktperson')
