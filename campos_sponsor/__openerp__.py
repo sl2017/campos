3c7d80d69cd50bc8ab73a4d1a194799648a2c56c
patbassing <patrick.bassing@gmail.com>
2015-12-19 23:14:35 +0100

    19122015 2313 version. Mangler: 1) Felter = res.partner 2) Alle kan redigere når den er potentiel, er den videre kan kun sponsorudvalg 3) kun udvalg kan se og bruge følgende knapper (se nedenfor) 4) Skjul udvalgsområde 5) Vedhæft filer 6) Rettigheder

--- campos_sponsor/__openerp__.py
+++ campos_sponsor/__openerp__.py
@@ -1,7 +1,7 @@
 {
 	'name': 'CampOS Sponsors',
 	'description': 'Administrer sponsorer og fonde med dette modul, udviklet til SL2017.',
-	'author': 'ITL',
+	'author': 'Patrick Bassing',
 	'depends': [
 			'mail',
 			'base',
--- campos_sponsor/model_sponsor.py
+++ campos_sponsor/model_sponsor.py
@@ -1,66 +1,135 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields
-from logging import PlaceHolder
-class TodoTask(models.Model):
+from openerp import models, fields, api
+
+class SponsorMain(models.Model):
 	_name = 'model.sponsor'
+	_inherit=['mail.thread', 'ir.needaction_mixin']
+	#_inherit={'res.partner':'partner_id'}
 	#Fields
+	#Fields for administrator
+	sponsor_adminnote = fields.Text('Udvalgsnote (skjult)', track_visibility='onchange',required=False)
+	#partner_id = fields.Many2one('res.partner', required=True, ondelete='restrict')
+	
 	#Fields ved oprettelse/forside
-	sponsor_name = fields.Char('Sponsor fond/firma', required=True)
-	sponsor_street = fields.Char('Vejnavn', required=True)
-	sponsor_city = fields.Char('By', required=True)
-	sponsor_zip = fields.Char('Postnummer', required=True)
-	sponsor_url = fields.Char('Webside', required=False)
-	sponsor_kontaktperson = fields.Many2one('res.partner','SL2017 kontaktperson', required=True)
+	sponsor_name = fields.Char('Sponsor fond/firma', track_visibility='onchange',required=True)
+	sponsor_street = fields.Char('Vejnavn', track_visibility='onchange',required=True)
+	sponsor_city = fields.Char('By', track_visibility='onchange',required=True)
+	sponsor_zip = fields.Char('Postnummer', track_visibility='onchange',required=True)
+	sponsor_cvr = fields.Char('CVR nr.', track_visibility='onchange',required=True)
+	sponsor_url = fields.Char('Webside', track_visibility='onchange',required=False)
+	sponsor_kontaktperson_sponsor = fields.Many2one('res.partner','Kontaktperson fra sponsor', track_visibility='onchange',required=True)
+	sponsor_kontaktperson_sl2017 = fields.Many2one('res.partner','SL2017 kontaktperson', track_visibility='onchange',required=True)
+	sponsor_udvalg_ansvarlig = fields.Many2one('campos.committee',
+                                   'Ansvarligt udvalg',
+                                   ondelete='set null',
+                                   track_visibility='onchange',
+                                   required=True)
 	sponsor_kategori = fields.Selection([('kategori_pengeinstitut', 'Pengeinstitut'),
 										('kategori_forsikring','Forsikring'),
-										('kategori_fodevare','Fødevarer'),
+										('kategori_fodevare',u'Fødevarer'),
 										('kategori_nonfood','Non-Food'),
 										('kategori_byggemarked','Byggemarked'),
 										('kategori_fonde','Fonde'),
 										('kategori_puljer','Puljer'),
 										('kategori_partner','Partner'),
-										('kategori_ovrige','Øvrige Sponsorer')],
+										('kategori_ovrige',u'Øvrige Sponsorer')],
 									'Sponsorat kategori',
+									track_visibility='onchange',
 									default='kategori_pengeinstitut',
 									required=True)
+	sponsor_temaer = fields.Many2many('sponsor.temaer', required=True)
+	'''
 	sponsor_tema = fields.Selection([('tema_spejder','Spejder og verden'),
 									('tema_demokrati','Unge og demokrati'),
 									('tema_teknologi','Teknologi og viden'),
-									('tema_natur','Natur og bæredygtighed'),
-									('tema_identitet','Identitet og grænseland'),
+									('tema_natur',u'Natur og bæredygtighed'),
+									('tema_identitet',u'Identitet og grænseland'),
 									('tema_mad','Mad og sundhed'),
-									('tema_samfund','Samfund og fællesskab')],
+									('tema_samfund',u'Samfund og fællesskab')],
 									'Sponsorat tema',
+									track_visibility='onchange',
 									default='tema_spejder',
 									required=True)
-	sponsor_ansogt = fields.Integer('Ansøgt værdi i DKK', required=True)
+									'''
+	sponsor_ansogt = fields.Integer(u'Ansøgt værdi i DKK', track_visibility='onchange',required=True)
+	sponsor_modydelser = fields.Many2many('sponsor.modydelser')
+	'''
 	sponsor_modydelse = fields.Selection([('modydelse_ingen','Ingen modydelse'),
 										('modydelse2','Modydelse 2')],
 										'Modydelse',
+										track_visibility='onchange',
 										default='modydelse_ingen')
-	sponsor_kommentar = fields.Text('Bemærkninger til sponsor')
+										'''
+	sponsor_kommentar = fields.Text(u'Bemærkninger til sponsor',track_visibility='onchange')
 	sponsor_type = fields.Selection([('type_reserveret', 'Reserveret Fond'),
 									('type_hoved', 'Hoved Sponsor'),
 									('type_event', 'Event eller Aktivitets Sponsor'),
 									('type_basis', 'Basis Sponsor')],
 								'Sponsor type',
-								default='type_basis',
-								required=True)
-	sponsor_type_begrund = fields.Text('Begrundelse for typevalg', required=True)
-	sponsor_udfyldtaf = fields.Many2one('res.partner','Udfyldt af', required=True)
+								track_visibility='onchange',
+								required=False)
+	sponsor_type_begrund = fields.Text('Begrundelse for typevalg', track_visibility='onchange')
+	sponsor_udfyldtaf = fields.Many2one('res.partner','Udfyldt af', track_visibility='onchange', required=True)
+	sponsor_bevilliget = fields.Integer(u'Bevilliget værdi', track_visibility='onchange', required=False)
 	
-	#Fields for administrator
-	sponsor_last = fields.Boolean('Sponsor Låst', default=False)
-	sponsor_last_begrund = fields.Text('låst begrundelse', required=False)
-	sponsor_bevilliget = fields.Integer('Bevilliget værdi', required=False)
-	sponsor_adminnote = fields.Char('Udvalgsnote (skjult)', required=False)
 	
 	#State
 	sponsor_state = fields.Selection([('state_potentiel','Potentiel'),
-									('state_ansogning','Ansøgning'),
-									('state_bevilling','Bevilling'),
-									('state_afvist','Afvist')],
+									('state_ansogning',u'Ansøgning'),
+									('state_bevilliget','Bevilling'),
+									('state_afvist','Afvist'),
+									('state_laast',u'Låst')],
 									track_visibility='onchange',
 									default='state_potentiel')
 	
 	
+	#KNAPPER
+	@api.one
+	def btn_opretansogning(self):
+		self.sponsor_state='state_ansogning'
+		
+	@api.one
+	def btn_bevillig(self):
+		self.sponsor_state='state_bevilliget'
+		
+	@api.one
+	def btn_afvis(self):
+		self.sponsor_state='state_afvist'
+		
+	@api.one
+	def btn_laas(self):
+		self.sponsor_state='state_laast'
+		
+	@api.one
+	def btn_aaben(self):
+		self.sponsor_state='state_potentiel'
+		
+		
+class SponsorTema(models.Model):
+	""" Temaer """
+	_description = 'Temaer'
+	_name = 'sponsor.temaer'
+	_order = 'name'
+	name = fields.Char('Name', size=64)
+	parent_id = fields.Many2one('sponsor.temaer', 'Parent Category', select=True, ondelete='cascade')
+	child_ids = fields.One2many('sponsor.temaer', 'parent_id', 'Child Categories')
+	active = fields.Boolean('Active', help="The active field allows you to hide the category without removing it.", default=True)
+	parent_left = fields.Integer('Left parent', select=True)
+	parent_right = fields.Integer('Right parent', select=True)    
+	_parent_store = True
+	_parent_order = 'name'
+		
+		
+class SponsorModydelse(models.Model):
+	""" Modydelser """
+	_description = 'Modydelser'
+	_name = 'sponsor.modydelser'
+	_order = 'name'
+	name = fields.Char('Name', size=64)
+	parent_id = fields.Many2one('sponsor.modydelser', 'Parent Category', select=True, ondelete='cascade')
+	child_ids = fields.One2many('sponsor.modydelser', 'parent_id', 'Child Categories')
+	active = fields.Boolean('Active', help="The active field allows you to hide the category without removing it.", default=True)
+	parent_left = fields.Integer('Left parent', select=True)
+	parent_right = fields.Integer('Right parent', select=True)    
+	_parent_store = True
+	_parent_order = 'name'
