2ee410c16b7f7c3914a2ab044f0c89e2ea24cd8d
patbassing <patrick.bassing@gmail.com>
2015-12-22 10:53:51 +0100

    No_create er sat på relevante felter Mindre form er fundet til kontaktperson for sponsor Flag er muligvis blevet oprettet, har ikke kunne gennemskue hvordan det testes, men har gjort følgende: -tilføjet sponsor = fields.Boolean() til res_partner.py i campos/modules -tilføjet følgende til sponsor-kontaktperson-field i view: 'default_sponsor':True, 'default_staff':False

--- campos_event/models/res_partner.py
+++ campos_event/models/res_partner.py
@@ -38,6 +38,7 @@ class ResPartner(models.Model):
     scoutgroup = fields.Boolean()
     participant = fields.Boolean()
     staff = fields.Boolean(default=True)
+    sponsor = fields.Boolean()
     skype = fields.Char()
     complete_contact = fields.Text("contact", compute='_get_complete_contact')
     
