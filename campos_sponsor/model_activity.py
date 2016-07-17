66da3f0e0e9c5d4d78b62b9d875c16c1277707bb
Patrick <pbas@itu.dk>
2016-07-17 13:53:06 +0200

    Aktivitetsportal kan indsende
    Aktivitetsportalen kan nu indsende ansøgning. Fejl skyldtes ">" i en
    tekststreng, så alle tekststrenge er lavet til unicode.
    Mangler email verifikation
    Mangler muligvis Transifex implementation?

--- campos_sponsor/model_activity.py
+++ campos_sponsor/model_activity.py
@@ -8,58 +8,58 @@ class ActivityMain(models.Model):
     
     activity_state = fields.Selection([('state_waiting',u'Afventer bekræftelse'),
                                     ('state_application',u'Ansøgning'),
-                                    ('state_processing','Behandles'),
-                                    ('state_approved','Godkendt'),
-                                    ('state_rejected','Afvist'),
+                                    ('state_processing',u'Behandles'),
+                                    ('state_approved',u'Godkendt'),
+                                    ('state_rejected',u'Afvist'),
                                     ('state_locked',u'Låst')],
                                     track_visibility='onchange',
                                     default='state_application')
     
     
     #Activity information
-    activity_name = fields.Char('Navn på aktivitet',track_visibility='onchange',required=True)
-    activity_groupname = fields.Char('Gruppenavn',track_visibility='onchange',required=True)
-    activity_description = fields.Text('Beskrivelse af aktivitet', track_visibility='onchange', required=True)
+    activity_name = fields.Char(u'Navn på aktivitet',track_visibility='onchange',required=True)
+    activity_groupname = fields.Char(u'Gruppenavn',track_visibility='onchange',required=True)
+    activity_description = fields.Text(u'Beskrivelse af aktivitet', track_visibility='onchange', required=True)
     activity_participant_usage = fields.Char(u'Hvad får spejderne ud af aktiviteten?',track_visibility='onchange', required=True)
     activity_participant_knowledge = fields.Char(u'Hvad lærer spejderne af aktiviteten?',track_visibility='onchange', required=True)
     
     
     #Contact person 1
-    activity_contact1_name = fields.Char('Navn',track_visibility='onchange',required=True)
-    activity_type = fields.Selection([('type_scout_group','Spedjergruppe'),
-                                      ('type_organisation','Organisation'),
-                                      ('type_company','Firma'),
-                                      ('type_private_person','Privatperson'),
-                                      ('type_other','Andet')]
+    activity_contact1_name = fields.Char(u'Navn',track_visibility='onchange',required=True)
+    activity_type = fields.Selection([('type_scout_group',u'Spedjergruppe'),
+                                      ('type_organisation',u'Organisation'),
+                                      ('type_company',u'Firma'),
+                                      ('type_private_person',u'Privatperson'),
+                                      ('type_other',u'Andet')]
                                      ,track_visibility='onchange',required=True)
-    activity_contact1_road = fields.Char('Vej',track_visibility='onchange',required=True)
-    activity_contact1_city = fields.Char('By',track_visibility='onchange',required=True)
-    activity_contact1_zip = fields.Char('Postnr',track_visibility='onchange',required=True)
-    activity_contact1_email = fields.Char('Email',track_visibility='onchange',required=True)
-    activity_contact1_tlf = fields.Char('Telefon',track_visibility='onchange',required=True)
+    activity_contact1_road = fields.Char(u'Vej',track_visibility='onchange',required=True)
+    activity_contact1_city = fields.Char(u'By',track_visibility='onchange',required=True)
+    activity_contact1_zip = fields.Char(u'Postnr',track_visibility='onchange',required=True)
+    activity_contact1_email = fields.Char(u'Email',track_visibility='onchange',required=True)
+    activity_contact1_tlf = fields.Char(u'Telefon',track_visibility='onchange',required=True)
     
     
     #Target audience
-    activity_age_from = fields.Char('Minimumsalder for aktivitet',track_visibility='onchange',required=True)
-    activity_age_to = fields.Char('Maximumsalder for aktivitet',track_visibility='onchange',required=True)
-    activity_expected_participants = fields.Selection([('expected_50','Under 50'),
-                                                       ('expected_50_100','50 - 100'),
-                                                       ('expected_101_200','101 - 200'),
-                                                       ('expected_201_350','201 - 350'),
-                                                       ('expected_351_500','351 - 500'),
-                                                       ('expected_501_750','501 - 750'),
-                                                       ('expected_751','>750')],
+    activity_age_from = fields.Char(u'Minimumsalder for aktivitet',track_visibility='onchange',required=True)
+    activity_age_to = fields.Char(u'Maximumsalder for aktivitet',track_visibility='onchange',required=True)
+    activity_expected_participants = fields.Selection([('expected_50',u'Under 50'),
+                                                       ('expected_50_100',u'50 - 100'),
+                                                       ('expected_101_200',u'101 - 200'),
+                                                       ('expected_201_350',u'201 - 350'),
+                                                       ('expected_351_500',u'351 - 500'),
+                                                       ('expected_501_750',u'501 - 750'),
+                                                       ('expected_751',u'>750')],
                                                        track_visibility='onchange', required=True)
     
     
     
     
     #Economy
-    activity_expense_total = fields.Char('Samlet udgift for aktivitet',track_visibility='onchange')
+    activity_expense_total = fields.Char(u'Samlet udgift for aktivitet',track_visibility='onchange')
     
     
     #Udvalgsnoter
-    activity_note = fields.Text('Noter til aktivitet', track_visibility='onchange')
+    activity_note = fields.Text(u'Noter til aktivitet', track_visibility='onchange')
     
     
     
