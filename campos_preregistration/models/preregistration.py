7aafd268a3e302a51c4770bf2144dc1949fd5b34
ernst <ernst@DESKTOP-549K4T8>
2016-08-30 21:59:43 +0200

    Transport setup changed to "own transport", model+views+security. View field security on Danish/international fields changed.

--- campos_preregistration/models/preregistration.py
+++ campos_preregistration/models/preregistration.py
@@ -10,7 +10,7 @@ class Preregistration(models.Model):
     group_association = fields.Many2one('campos.scout.org','Scout Organization')
     group_world_association = fields.Selection(related='group_association.worldorg', string='World Organisation', readonly=True)
     group_entrypoint = fields.Char('Point of entry into Denmark')
-    group_municipality = fields.Many2one('campos.municipality','Municipallity for DK groups / Place of arrival for non DK groups')
+    group_municipality = fields.Many2one('campos.municipality','Municipallity')
     group_country = fields.Many2one('res.country', 'Country')
     group_country_name = fields.Char(related='group_country.name', string='Country Name', readonly=True)
     association_groupid = fields.Char('Groups id (number) at local association')
@@ -36,6 +36,10 @@ class PreregistrationPioneeringPole(models.Model):
     name = fields.Char('Pioneering Pole Name', required=True)
     length = fields.Integer('Length', required=True)
 
+class PreregistrationTransportType(models.Model):
+    _name = 'event.registration.transporttype'
+    name = fields.Char('Transport Type Name', required=True)
+
 class PreregistrationParticipants(models.Model):
     _name = 'event.registration.participants'
     registration_id = fields.Many2one('event.registration', 'Registration')
@@ -43,26 +47,43 @@ class PreregistrationParticipants(models.Model):
     participant_total  = fields.Integer('Number of participants', required=True)
     participant_from_date = fields.Date('Date of arrival', required=True, default='2017-07-22')
     participant_to_date = fields.Date('Date of departure', required=True, default='2017-07-30')
-    participant_transport_to_camp_total  = fields.Integer('Number of transport to camp', required=True)
-    participant_transport_from_camp_total  = fields.Integer('Number of transport from camp', required=True)
-    participant_transport_note = fields.Char(compute = '_calculate_note', string='Note')
+    participant_own_transport_to_camp_total  = fields.Integer('No. own transport to camp', required=True)
+    participant_own_transport_from_camp_total  = fields.Integer('No. own transport from camp', required=True)
+    participant_common_transport_to_camp_total  = fields.Integer(compute = '_calculate_common_transport', string='No. Common transport to camp')
+    participant_common_transport_from_camp_total  = fields.Integer(compute = '_calculate_common_transport', string='No. Common transport from camp')
+    participant_own_transport_type = fields.Many2one('event.registration.transporttype','Primary own transport')
     
-    @api.depends ('participant_total','participant_transport_to_camp_total','participant_transport_from_camp_total')
-    @api.multi
-    def _calculate_note (self):
-        for record in self:
-            if record.participant_transport_to_camp_total<record.participant_total or record.participant_transport_from_camp_total<record.participant_total:
-                record.participant_transport_note = 'Not all participants with transport.'
-            if record.participant_transport_to_camp_total>record.participant_total or record.participant_transport_from_camp_total>record.participant_total:
-                record.participant_transport_note = 'More than all participants with transport.'
+#    participant_transport_note = fields.Char(compute = '_calculate_note', string='Note')
+    
+#    @api.depends ('participant_total','participant_transport_to_camp_total','participant_transport_from_camp_total')
+#    @api.multi
+#    def _calculate_note (self):
+#        for record in self:
+#            if record.participant_transport_to_camp_total<record.participant_total or record.participant_transport_from_camp_total<record.participant_total:
+#                record.participant_transport_note = 'Not all participants with transport.'
+#            if record.participant_transport_to_camp_total>record.participant_total or record.participant_transport_from_camp_total>record.participant_total:
+#                record.participant_transport_note = 'More than all participants with transport.'
                 
+    @api.depends ('participant_total','participant_own_transport_to_camp_total','participant_own_transport_from_camp_total')
+    @api.one
+    def _calculate_common_transport (self):
+        for record in self:
+            record.participant_common_transport_to_camp_total = record.participant_total - record.participant_own_transport_to_camp_total
+            record.participant_common_transport_from_camp_total = record.participant_total - record.participant_own_transport_from_camp_total
                 
     def _check_from_before_end(self):
         if self.participant_from_date < self.participant_to_date:
             return True
         return False
     
-    
+    @api.one
+    @api.constrains('participant_total','participant_own_transport_to_camp_total','participant_own_transport_from_camp_total')
+    def validation_own_transport_numbers(self):
+        if self.participant_own_transport_to_camp_total>self.participant_total:
+            raise exceptions.ValidationError('Own transport to camp more than total participants')
+        if self.participant_own_transport_from_camp_total>self.participant_total:
+            raise exceptions.ValidationError('Own transport from camp more than total participants')
+
     @api.one
     @api.constrains('participant_from_date', 'participant_to_date')
     def validation_from_to_dates(self):
@@ -87,3 +108,4 @@ class PreregistrationPolelist(models.Model):
     registration_id = fields.Many2one('event.registration', 'Registration')
     pioneeringpole_id = fields.Many2one('event.registration.pioneeringpole','Pole type', required=True)
     polecount  = fields.Integer('Number of pioneering poles', required=True)
+
