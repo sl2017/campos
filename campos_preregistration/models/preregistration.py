a45ea91e8fde7828ea949ab1f852bf8773872555
ernst <ernst@DESKTOP-549K4T8>
2016-09-19 21:11:54 +0200

    Removed unused "group_name" from model. Hide handicap related fields when handicap not selected. Display field labels on tabs "Agreements", "Sub Camp" and "Internal Information".

--- campos_preregistration/models/preregistration.py
+++ campos_preregistration/models/preregistration.py
@@ -6,7 +6,7 @@ class Preregistration(models.Model):
     Pre-registration for a scout group to an event
     '''
     _inherit = 'event.registration'
-    group_name = fields.Char('Group Name')
+#    group_name = fields.Char('Group Name')
 #    group_association = fields.Many2one('campos.scout.org','Scout Organization')
 #    group_world_association = fields.Selection(related='group_association.worldorg', string='World Organisation', readonly=True)
     group_entrypoint = fields.Char('Point of entry into Denmark')
