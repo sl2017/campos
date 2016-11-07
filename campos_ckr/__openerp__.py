9ad2846004b3b13547355d7f4befc389087f9def
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2016-11-07 16:43:51 +0100

    Task1162: Request Copy Wizard

---
+++ campos_ckr/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Stein & Gabelgaard ApS
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Campos CKR',
+    'description': """
+        CKR handling""",
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Stein & Gabelgaard ApS',
+    'website': 'www.steingabelgaard.dk',
+    'depends': ['campos_event', 'model_field_access'
+    ],
+    'data': [
+        'wizards/campos_ckr_sentin_wiz.xml',
+        'security/campos_ckr_check.xml',
+        'views/campos_ckr_check.xml',
+    ],
+    'demo': [
+    ],
+}
