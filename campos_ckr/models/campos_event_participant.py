b4ec10bceb99e990203ff13c8380ad1b829a25e3
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2016-11-08 17:14:42 +0100

    CKR

---
+++ campos_ckr/models/campos_event_participant.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Stein & Gabelgaard ApS
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models, _
+
+
+class CamposEventParticipant(models.Model):
+
+    _inherit = 'campos.event.participant'
+    
+    ckr_ids = fields.One2many('campos.ckr.check', 'participant_id', string='CKR')
