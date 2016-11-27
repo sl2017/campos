377c888e61007d9e801cf53de05cb2ca1d6bf6af
ernst <ernst@DESKTOP-549K4T8>
2016-11-27 22:27:21 +0100

    Final Registration. Model update.

--- campos_final_registration/models/finalregistration.py
+++ campos_final_registration/models/finalregistration.py
@@ -6,12 +6,12 @@ class FinalRegistration(models.Model):
     Final registration for a scout group to an event
     '''
     _inherit = 'event.registration'
-#  **********shouldn't really be integer**********
-    total_price = fields.Integer('Total Price')
+    total_price = fields.Float('Total Price')
     child_certificates_accept = fields.Boolean('We declare to not bring any leaders not having a child certificate')
     child_certificates_date = fields.Date('Date of declaration') 
     child_certificates_user = fields.Char('User signing declaration')
     friendhip_group_ids = fields.One2many('event.registration.friendshipgrouplist','registration_id','Friendship Groups')
+    pioneering_pole_depot = fields.Char('Pioneering Pole Depot (ref. to depot?)')
     
     
 class FriendshipGroupList(models.Model):
@@ -26,16 +26,17 @@ class FinalRegistrationParticipant(models.Model):
     _inherit = 'campos.event.participant'
     has_allergy_gluten = fields.Boolean('Has Gluten Allergy')
     has_allergy_milk = fields.Boolean('Has Milk Allergy')
+    # Other allergies???
     own_transport_to_camp = fields.Boolean('No common transport TO camp')
     own_transport_from_camp = fields.Boolean('No common transport FROM camp')
-#    camp_day_ids = fields.One2many('campos.event.participant.day','partner_id','Camp Day List')
+    camp_day_ids = fields.One2many('campos.event.participant.day','participant_id','Camp Day List')
     reside_other_group_id = fields.Many2one('res.partner', 'Resides with this group')
     reside_in_caravan =fields.Char('Resides in caravan?!?!')
     access_token_id = fields.Char('Id of Access Token')
     
 class ParticipantCampDay(models.Model):
     '''
-    Extending event participant for final registration
+    One persons participation to a camp in one day
     '''
     _name = 'campos.event.participant.day'
     participant_id = fields.Many2one('campos.event.participant', 'Participant')
