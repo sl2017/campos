cd7e763785778bb3ff3160d4e1df97f7d4582198
Patrick Bassing <Patrick Bassing>
2017-03-04 00:16:46 +0100

    Basic model and GUI Basic model for network registration created. Missing "udvalg". Basic registration web form created with camp card in iframe. TODO: Make post save model. Controllers class currently only has get method. Post to come. Security is on the todo list.

---
+++ campos_network/__openerp__.py
@@ -0,0 +1,14 @@
+{
+	'name': 'CampOS Network',
+	'description': 'Lader udvalg bestille netvaerk paa bestemte koordinater.',
+	'author': 'Patrick Bassing',
+	'depends': [
+			'mail',
+			'base',
+			'campos_event',
+			],
+	'application': True,
+	'data': [
+        'createnetwork_template.xml',
+        ],
+}
---
+++ campos_network/model_network.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+class NetworkMain(models.Model):
+	_name = 'model.network'
+	_inherit=['mail.thread', 'ir.needaction_mixin']
+    
+    
+	network_state = fields.Selection([('state_ordered',u'Bestilt'),
+									('state_processing',u'Behandles'),
+									('state_approved',u'Godkendt'),
+									('state_rejected',u'Afvist')],
+									track_visibility='onchange',
+									default='state_application')
+									
+									
+	#Ansøger
+	#TODO Inkluder udvalg
+	network_name = fields.Char(u'Navn', track_visibility='onchance', required=True)
+	network_email = fields.Char(u'Email', track_visibility='onchance', required=True)
+	network_phone = fields.Char(u'Telefon', track_visibility='onchance', required=True)
+	
+	
+	#Behov
+	network_connection = fields.Selection([('type_network_wire',u'Kablet'),
+                                      ('type_network_wireless',u'Trådløs'),
+                                      ('type_network_both',u'Begge')]
+                                     ,track_visibility='onchange',required=True)
+	network_usage = fields.Char(u'Anvendelse', track_visibility='onchance', required=True)
+	
+	
+	#Hvornår
+	#Leveringsdato
+	network_delivery_day = fields.Selection([('network_delivery_day_01',u'01'),
+                                      ('network_delivery_day_02',u'02'),
+                                      ('network_delivery_day_03',u'03'),
+                                      ('network_delivery_day_04',u'04'),
+                                      ('network_delivery_day_05',u'05'),
+                                      ('network_delivery_day_06',u'06'),
+                                      ('network_delivery_day_07',u'07'),
+                                      ('network_delivery_day_08',u'08'),
+                                      ('network_delivery_day_09',u'09'),
+                                      ('network_delivery_day_10',u'10'),
+                                      ('network_delivery_day_11',u'11'),
+                                      ('network_delivery_day_12',u'12'),
+                                      ('network_delivery_day_13',u'13'),
+                                      ('network_delivery_day_14',u'14'),
+                                      ('network_delivery_day_15',u'15'),
+                                      ('network_delivery_day_16',u'16'),
+                                      ('network_delivery_day_17',u'17'),
+                                      ('network_delivery_day_18',u'18'),
+                                      ('network_delivery_day_19',u'19'),
+                                      ('network_delivery_day_20',u'20'),
+                                      ('network_delivery_day_21',u'21'),
+                                      ('network_delivery_day_22',u'22'),
+                                      ('network_delivery_day_23',u'23'),
+                                      ('network_delivery_day_24',u'24'),
+                                      ('network_delivery_day_25',u'25'),
+                                      ('network_delivery_day_26',u'26'),
+                                      ('network_delivery_day_27',u'27'),
+                                      ('network_delivery_day_28',u'28'),
+                                      ('network_delivery_day_29',u'29'),
+                                      ('network_delivery_day_30',u'30'),
+                                      ('network_delivery_day_31',u'31')]
+                                     ,track_visibility='onchange',required=True)
+	network_delivery_month = fields.Selection([('network_delivery_month_apr',u'April'),
+                                      ('network_delivery_month_maj',u'Maj'),
+                                      ('network_delivery_month_jun',u'Juni'),
+                                      ('network_delivery_month_jul',u'Juli')]
+                                     ,track_visibility='onchange',required=True)
+	#Ibrugtagelse
+	network_usage_day = fields.Selection([('network_usage_day_01',u'01'),
+                                      ('network_usage_day_02',u'02'),
+                                      ('network_usage_day_03',u'03'),
+                                      ('network_usage_day_04',u'04'),
+                                      ('network_usage_day_05',u'05'),
+                                      ('network_usage_day_06',u'06'),
+                                      ('network_usage_day_07',u'07'),
+                                      ('network_usage_day_08',u'08'),
+                                      ('network_usage_day_09',u'09'),
+                                      ('network_usage_day_10',u'10'),
+                                      ('network_usage_day_11',u'11'),
+                                      ('network_usage_day_12',u'12'),
+                                      ('network_usage_day_13',u'13'),
+                                      ('network_usage_day_14',u'14'),
+                                      ('network_usage_day_15',u'15'),
+                                      ('network_usage_day_16',u'16'),
+                                      ('network_usage_day_17',u'17'),
+                                      ('network_usage_day_18',u'18'),
+                                      ('network_usage_day_19',u'19'),
+                                      ('network_usage_day_20',u'20'),
+                                      ('network_usage_day_21',u'21'),
+                                      ('network_usage_day_22',u'22'),
+                                      ('network_usage_day_23',u'23'),
+                                      ('network_usage_day_24',u'24'),
+                                      ('network_usage_day_25',u'25'),
+                                      ('network_usage_day_26',u'26'),
+                                      ('network_usage_day_27',u'27'),
+                                      ('network_usage_day_28',u'28'),
+                                      ('network_usage_day_29',u'29'),
+                                      ('network_usage_day_30',u'30'),
+                                      ('network_usage_day_31',u'31')]
+                                     ,track_visibility='onchange',required=True)
+	network_usage_month = fields.Selection([('network_usage_month_apr',u'April'),
+                                      ('network_usage_month_maj',u'Maj'),
+                                      ('network_usage_month_jun',u'Juni'),
+                                      ('network_usage_month_jul',u'Juli')]
+                                     ,track_visibility='onchange',required=True)
+	#Sidste anvendelse
+	network_last_day = fields.Selection([('network_last_day_01',u'01'),
+                                      ('network_last_day_02',u'02'),
+                                      ('network_last_day_03',u'03'),
+                                      ('network_last_day_04',u'04'),
+                                      ('network_last_day_05',u'05'),
+                                      ('network_last_day_06',u'06'),
+                                      ('network_last_day_07',u'07'),
+                                      ('network_last_day_08',u'08'),
+                                      ('network_last_day_09',u'09'),
+                                      ('network_last_day_10',u'10'),
+                                      ('network_last_day_11',u'11'),
+                                      ('network_last_day_12',u'12'),
+                                      ('network_last_day_13',u'13'),
+                                      ('network_last_day_14',u'14'),
+                                      ('network_last_day_15',u'15'),
+                                      ('network_last_day_16',u'16'),
+                                      ('network_last_day_17',u'17'),
+                                      ('network_last_day_18',u'18'),
+                                      ('network_last_day_19',u'19'),
+                                      ('network_last_day_20',u'20'),
+                                      ('network_last_day_21',u'21'),
+                                      ('network_last_day_22',u'22'),
+                                      ('network_last_day_23',u'23'),
+                                      ('network_last_day_24',u'24'),
+                                      ('network_last_day_25',u'25'),
+                                      ('network_last_day_26',u'26'),
+                                      ('network_last_day_27',u'27'),
+                                      ('network_last_day_28',u'28'),
+                                      ('network_last_day_29',u'29'),
+                                      ('network_last_day_30',u'30'),
+                                      ('network_last_day_31',u'31')]
+                                     ,track_visibility='onchange',required=True)
+	network_last_month = fields.Selection([('network_last_month_apr',u'April'),
+                                      ('network_last_month_maj',u'Maj'),
+                                      ('network_last_month_jun',u'Juni'),
+                                      ('network_last_month_jul',u'Juli')]
+                                     ,track_visibility='onchange',required=True)
+	
+	
+	
+	#Hvor
+	network_lat = fields.Char(u'Latitude/breddegrad',track_visibility='onchange',required=True)
+	network_lon = fields.Char(u'Longitude/længdegrad',track_visibility='onchange',required=True)
+	
+	
+	#Andet
+	network_other = fields.Text(u'Evt. bemærkninger', track_visibility='onchange')
+	
+	
+	#Admin
+	network_admin_notes = fields.Text(u'Admin noter', track_visibility='onchange')
+	
+	
+	
+    
+    #BUTTONS
+	@api.one
+	def btn_network_order(self):
+		self.network_state='state_ordered'
+	
+	@api.one
+	def btn_network_processing(self):
+		self.network_state='state_processing'
+		
+	@api.one
+	def btn_network_confirm(self):
+		self.network_state='state_approved'
+		
+	@api.one
+	def btn_network_reject(self):
+		self.network_state='state_rejected'
