5e1e050e74397dc26d66a228b9e9af8b77479518
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2017-03-31 22:25:14 +0200

    Initial datamodel fro activities from E2014

--- campos_activity/models/campos_activity_activity.py
+++ campos_activity/models/campos_activity_activity.py
@@ -8,6 +8,17 @@ from openerp import api, fields, models, _
 class CamposActivityActivity(models.Model):
 
     _name = 'campos.activity.activity'
-    _description = 'Campos Activity Activity'  # TODO
-
-    name = fields.Char()
+    _description = 'Campos Activity'
+    _order = 'name'
+    _inherit = 'mail.thread'
+    
+    name = fields.Char('Name', size=128, translate=True)
+    committee_id = fields.Many2one('campos.committee', 'Committee')
+    desc = fields.Text('Description', translate=True)
+    age_from =  fields.Integer('Age from', default=0)
+    age_to = fields.Integer('Age to', default=0)
+    #    'points' : fields.integer('Points'),
+    audience = fields.Selection([('par','Participants'),
+                                 ('staff','ITS'),
+                                 ('all', 'All')], 'Audience', default='par')
+    act_ins_ids = fields.One2many('campos.activity.instanse', 'activity_id', 'Instanses')
--- campos_activity/models/campos_activity_period.py
+++ campos_activity/models/campos_activity_period.py
@@ -8,6 +8,8 @@ from openerp import api, fields, models, _
 class CamposActivityPeriod(models.Model):
 
     _name = 'campos.activity.period'
-    _description = 'Campos Activity Period'  # TODO
+    _description = 'Campos Activity Period'
 
-    name = fields.Char()
+    name = fields.Char('Name', size=128, translate=True)
+    date_begin = fields.Datetime('Start Date/Time', required=True),
+    date_end = fields.Datetime('End Date/Time', required=True), 
