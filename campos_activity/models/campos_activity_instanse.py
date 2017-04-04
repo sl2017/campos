f32709a6cc0e9982aa92da9d499cc2a3bb6f4aa9
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2017-04-04 09:40:40 +0200

    pylint

--- campos_activity/models/campos_activity_instanse.py
+++ campos_activity/models/campos_activity_instanse.py
@@ -18,8 +18,8 @@ class CamposActivityInstanse(models.Model):
     #    'seats_available': fields.function(_get_seats, string='Available Seats', type='integer', multi='seats_reserved', fnct_search=_search_seats),
     #    'seats_used': fields.function(_get_seats, string='Number of Participations', type='integer', multi='seats_reserved'),
     #'complete_name': fields.function(_name_get_fnc, type="char", string='Full Name', multi='seats_reserved'),
-    activity_id = fields.Many2one('campos.activity.activity', 'Activity'),
-    period_id =  fields.Many2one('campos.activity.period', 'Period'),
+    activity_id = fields.Many2one('campos.activity.activity', 'Activity')
+    period_id = fields.Many2one('campos.activity.period', 'Period')
     staff_ids = fields.Many2many('campos.event.participant','campos_activity_staff_rel', 'act_ins_id','par_id','Staff')
     ticket_ids = fields.One2many('campos.activity.ticket', 'act_ins_id', 'Tickets')
     actins_date_begin = fields.Datetime(related='period_id.date_begin', string='Start Date/Time', store=True)
