3766ab7b1833277c085f80917c752db34af6e4af
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2017-04-19 19:54:25 +0200

    Recalc and confirm sales orders

--- campos_import/models/sale_order.py
+++ campos_import/models/sale_order.py
@@ -8,3 +8,35 @@ from openerp import api, fields, models, _
 class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
+    
+    @api.multi
+    def reset_lines(self, price=False):
+        """
+        Reset lines according informations on products and price list
+        :param price: boolean to indicate if we are resetting price or
+                      descriptions
+        """
+        for line in self.mapped('order_line'):
+            order = line.order_id
+            res = line.product_id_change(
+                order.pricelist_id.id, line.product_id.id,
+                qty=line.product_uom_qty, uom=line.product_uom.id,
+                qty_uos=line.product_uos_qty, uos=line.product_uos.id,
+                name=line.name, partner_id=order.partner_id.id, lang=False,
+                update_tax=True, date_order=order.date_order, packaging=False,
+                fiscal_position=order.fiscal_position.id, flag=price)
+            if price:
+                line.write(res['value'])
+            else:
+                if 'name' in res['value']:
+                    line.write({'name': res['value']['name']})
+        return True
+    
+    @api.multi
+    def confirm_orders(self):
+        for order in self:
+            order.reset_lines(price=True)
+            order.action_button_confirm()
+        return True
+            
+    
