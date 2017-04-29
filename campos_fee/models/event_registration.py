dd31f141e8e2f7184b90d81b2d33f51c07c5b637
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2017-04-29 23:10:36 +0200

    # of participants....

--- campos_fee/models/event_registration.py
+++ campos_fee/models/event_registration.py
@@ -52,25 +52,23 @@ class EventRegistration(models.Model):
     @api.depends('participant_ids', 'participant_ids.state')
     def _compute_fees(self):
         
-        if self.env.uid == SUPERUSER_ID:
-            s = self
-        else:
-            s = self.suspend_security()
-        for reg in s:
-            _logger.info('INit Calc')
+        for reg in self:
             fee_participants = 0.0
             fee_transport = 0.0
             number_participants = 0
-            for par in reg.participant_ids.filtered(lambda r: r.state not in ['cancel', 'deregistered']):
+            if self.env.uid == SUPERUSER_ID:
+                pars = reg.participant_ids.filtered(lambda r: r.state not in ['cancel', 'deregistered'])
+            else:
+                pars = reg.participant_ids.suspend_security().filtered(lambda r: r.state not in ['cancel', 'deregistered'])
+            for par in pars:
                 fee_participants += par.camp_price
                 fee_transport += par.transport_price_total
                 number_participants += 1
-                _logger.info('Calc loop %s', par.name)
             reg.fee_participants = fee_participants
             reg.fee_transport = fee_transport
             reg.number_participants = number_participants
             reg.number_participants_stored = number_participants
-            _logger.info('Calc # %d', number_participants)
+            _logger.info('Calc # %d %s', number_participants, reg.name)
             so_cost = 0.0
             if self.env.uid == SUPERUSER_ID:
                 for so in self.env['sale.order.line'].search([('order_partner_id', '=', reg.partner_id.id),('order_id.state', '!=', 'cancel')]):
