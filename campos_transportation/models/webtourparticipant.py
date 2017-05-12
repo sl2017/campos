58457e07064ac877b5e0baea851246f1e3ead1ce
jda.dk <jda.dk@045-742.WindowMaster.org>
2017-05-13 00:35:15 +0200

    Clear Jobber Campdays improved

--- campos_transportation/models/webtourparticipant.py
+++ campos_transportation/models/webtourparticipant.py
@@ -558,14 +558,20 @@ class WebtourParticipant(models.Model):
 
     @api.one
     def clearusecamptransportjobber_nocampdays(self):
-        _logger.info("%s action_clearusecamptransportjobber_nocampdays to camp %s %s %s", self.id,self.transport_to_camp,self.transport_from_camp,self.camp_day_ids)
+        _logger.info("%s action_clearusecamptransportjobber_nocampdays to camp %s %s %s %s", self.id,self.transport_to_camp,self.transport_from_camp,self.dates_summery)
         
-        if self.staff and not self.camp_day_ids and self.transport_to_camp:            
-            self.transport_to_camp = False
+        willcome = False
+        for d in self.camp_day_ids:
+                if d.will_participate: willcome=willcome + 1
+        
+        if willcome:
+            _logger.info("clearusecamptransportjobber_nocampdays VERY STRANGE %s",willcome)
+        else:
+            if self.staff and self.transport_to_camp:    
+                self.transport_to_camp = False
             
-        if self.staff and not self.camp_day_ids and self.transport_from_camp:   
-            self.transport_from_camp = False
-        
+            if self.staff and self.transport_from_camp:
+                self.transport_from_camp = False
         return
     
              
