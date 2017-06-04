6f0aebb61ef049fad46f4ac029e7e099583b0274
jda.dk <jda.dk@045-742.WindowMaster.org>
2017-06-04 22:39:17 +0200

    date user id added to sql view. Last mail added to tickets

--- campos_transportation/interface/webtour_testJDa.py
+++ campos_transportation/interface/webtour_testJDa.py
@@ -2,6 +2,7 @@ print "Start."
 
 import datetime
 import webtourinterface
+import xml.etree.ElementTree as ET
 
 current_time = datetime.datetime.now().time()
 print current_time.isoformat()
@@ -15,9 +16,22 @@ request=request+"&EndDestinationIDno="+"4391"
 request=request+"&EndDateTime="+"2016-12-27"
 request=request+"&EndNote="+"bb"
 
-response_doc=webtourinterface.usneed_GetPending_ByIDno('https://services.techhouse.dk/webTourManager/1.5/SL2017.svc/rest/xml/','Login?OwnerIDno=107&Alias=testonly&AuthKey=F3ACDD1A-046D-42D7-A3B8-8A20488AA016','221051')
+response_doc=webtourinterface.usneed_getbyidno('https://services.techhouse.dk/webTourManager/1.7/SL2017.svc/rest/xml/','Login?OwnerIDno=107&Alias=testonly&AuthKey=F3ACDD1A-046D-42D7-A3B8-8A20488AA016','306512')
 
-usContent = response_doc.getElementsByTagName("Content")  
+response_root = ET.fromstring(webtourinterface.usneed_getbyidno1('https://services.techhouse.dk/webTourManager/1.7/SL2017.svc/rest/xml/','Login?OwnerIDno=107&Alias=testonly&AuthKey=F3ACDD1A-046D-42D7-A3B8-8A20488AA016','306512'))           
+print response_root
+print response_root.tag
+ns = {'i': 'http://schemas.datacontract.org/2004/07/webTourManager',
+      'a': 'http://schemas.datacontract.org/2004/07/webTourManager.Classes'}
+
+content = response_root.find("i:Content",ns)
+print content.text
+
+for child in content:
+    print child.tag
+
+id = content.find("a:IDno",ns)
+print id.text
 
 element = usContent[0]
 
