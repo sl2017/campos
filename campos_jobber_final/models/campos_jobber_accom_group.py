c06388be98f991d03427237367bf8c6ebd03c260
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2017-07-05 16:47:06 +0200

    Avoid doublets in accomondation code

--- campos_jobber_final/models/campos_jobber_accom_group.py
+++ campos_jobber_final/models/campos_jobber_accom_group.py
@@ -10,12 +10,18 @@ class CamposJobberAccomGroup(models.Model):
     _name = 'campos.jobber.accom.group'
     _description = 'Campos Jobber Accom Group'  # TODO
 
-    name = fields.Char()
-    code = fields.Char()
+    name = fields.Char(required=True)
+    code = fields.Char(required=True)
     owner_id = fields.Many2one('campos.event.participant', 'Owner')
     accom_participant_ids = fields.One2many('campos.jobber.accomodation', 'accom_group_id', string='Participants')
     number_participants = fields.Integer('# participants', compute='_compute_number_participants')
     subcamp_id = fields.Many2one('campos.subcamp', 'Sub Camp')
+    
+    _sql_constraints = [
+                        ('code_uniq', 'unique(code)', 'Code already in use. Choose another'),
+                        ('name_uniq', 'unique(name)', 'Name already in use. Choose another'),
+                       ]
+    
 
     @api.depends('accom_participant_ids')
     @api.multi
