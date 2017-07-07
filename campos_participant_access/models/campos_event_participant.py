569f78b3013504012140009b527fe0e785e632ce
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2017-07-07 15:42:49 +0200

    Track Moodle status

--- campos_participant_access/models/campos_event_participant.py
+++ campos_participant_access/models/campos_event_participant.py
@@ -4,12 +4,75 @@
 
 from openerp import api, fields, models, exceptions, _
 
+from xml.etree import ElementTree as ET
+from moodle_rest_api import MDL
 
 class CamposEventParticipant(models.Model):
 
     _inherit = 'campos.event.participant'
 
     clc_user_needed = fields.Boolean('CLC User needed', compute='_compute_clc_user_needed')
+    clc_state = fields.Selection([('required', 'Required'),
+                                  ('enrolled', 'Enrolled'),
+                                  ('passed', 'Passed')], string='CLC state')
+    clc_userid = fields.Integer('Moodle Internal User ID')
+    clc_grade = fields.Char('CLC Total Grade')
+    
+    
+    def moodle_get_grade(self):
+        mdl = MDL()
+
+        server = eval(self.env['ir.config_parameter'].get_param('campos.clc_server'))
+        params = {'userid': self.clc_userid}
+
+        grades = mdl.get_course_grades(server, params)
+        rows = ET.ElementTree(ET.fromstring(grades))
+        for row in rows.getroot():
+            for key0 in row:
+                print key0
+                if key0.tag == 'KEY' and key0.attrib.get('name') == 'grades':
+                    for multiple in key0:
+                        for single in multiple:
+                            for key in single:
+                                if key.tag == 'KEY' and key.attrib.get('name') == 'grade':
+                                    val = dict((e.tag, e.text) for e in key)
+                                    self.clc_grade = (val['VALUE'])
+                                    if self.clc_grade == '400.00':
+                                        self.clc_state = 'passed'
+
+
+    @api.model
+    def moodle_get_course_users(self):
+        mdl = MDL()
+        course = eval(self.env['ir.config_parameter'].get_param('campos.clc_course_id'))
+        server = eval(self.env['ir.config_parameter'].get_param('campos.clc_server'))
+        params = {'courseid': course}
+        mdl_users = mdl.get_course_users(server, params)
+        rows = ET.ElementTree(ET.fromstring(mdl_users))
+        for row in rows.getroot():
+            for single in row:
+                moodle_id = False
+                moodle_username = False
+                for key in single:
+                    if key.tag == 'KEY' and key.attrib.get('name') == 'id':
+                        val = dict((e.tag, e.text) for e in key)
+                        moodle_id = val['VALUE']
+                    if key.tag == 'KEY' and key.attrib.get('name') == 'username':
+                        val = dict((e.tag, e.text) for e in key)
+                        moodle_username = val['VALUE']
+                if moodle_id and moodle_username:
+                    usr = self.env['res.users'].search([('login', '=', moodle_username)])
+                    if usr:
+                        if not usr.participant_id:
+                            part = self.env['campos.event.participant'].search([('partner_id', '=', usr.partner_id.id)])
+                            if part:
+                                usr.participant_id = part
+                        else:
+                            part = usr.participant_id
+                        if part and not part.clc_userid:
+                            part.clc_userid = moodle_id
+                            part.clc_state = 'enrolled'
+                        part.moodle_get_grade()
     
     @api.multi
     def _compute_clc_user_needed(self):
