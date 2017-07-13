9d2c57f48b97d8c5e9c50cf0999c6c91d901b985
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2017-07-13 11:33:05 +0200

    Don't include "No invoicing" in total fee

--- campos_fee/models/event_registration.py
+++ campos_fee/models/event_registration.py
@@ -64,8 +64,9 @@ class EventRegistration(models.Model):
             else:
                 pars = reg.participant_ids.suspend_security().filtered(lambda r: r.state not in ['cancel', 'deregistered'])
             for par in pars:
-                fee_participants += par.camp_price
-                fee_transport += par.transport_price_total
+                if not par.no_invoicing:
+                    fee_participants += par.camp_price
+                    fee_transport += par.transport_price_total
                 number_participants += 1
                 if not par.staff:
                     number_accomondations += 1
