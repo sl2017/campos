85f86aade0e0a94a17bb39323dbcaf0889ccce83
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2017-07-16 22:06:38 +0200

    Don't subtract refusion charge from last invoice

--- campos_fee/models/campos_fee_ss_registration.py
+++ campos_fee/models/campos_fee_ss_registration.py
@@ -276,10 +276,11 @@ class CamposFeeSsRegistration(models.Model):
                 self.env['account.invoice.line'].create(vals)
                 for line in ssreg.ref_ssreg_id.invoice_id.invoice_line:
                     if line.product_id.default_code and line.product_id.default_code.startswith('LK') and line.quantity > 0:
-                        vals = self._prepare_create_invoice_line_vals(line.price_unit  if ssreg.ref_ssreg_id.invoice_id.type == 'out_invoice' else -line.price_unit, -line.quantity, type='out_invoice', description=line.name, product=line.product_id)
-                        vals['invoice_id'] = ssreg.invoice_id.id
-                        self.env['account.invoice.line'].create(vals)
-                    
+                        if line.product_id.default_code != 'LKREF':
+                            vals = self._prepare_create_invoice_line_vals(line.price_unit  if ssreg.ref_ssreg_id.invoice_id.type == 'out_invoice' else -line.price_unit, -line.quantity, type='out_invoice', description=line.name, product=line.product_id)
+                            vals['invoice_id'] = ssreg.invoice_id.id
+                            self.env['account.invoice.line'].create(vals)
+                        
                 # Handle "no_cancel_fee
                 cancelled_fee, cancelled_transport = ssreg._get_no_cancel_fee()    
                 charged_fee_par = charged_fee_par - cancelled_fee
