49c6b0acd9c46c9723e73f593fb0d894278d5588
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2017-07-16 11:56:42 +0200

    3th party depends

--- campos_rfid/__openerp__.py
+++ campos_rfid/__openerp__.py
@@ -11,6 +11,9 @@
     'author': 'Stein & Gabelgaard ApS',
     'website': 'www.steingabelgaard.dk',
     'depends': ['mail', 'campos_jobber_final'
+                'web_action_request',
+                'web_widget_x2many_barchart',
+                
     ],
     'data': [
         'views/campos_event_participant.xml',
