62dcc55e2f0c02faf866b67f3c1d0d011a69674a
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2017-07-21 23:23:39 +0200

    Due import

--- campos_checkin/wizards/campos_import_grp_due_wiz.py
+++ campos_checkin/wizards/campos_import_grp_due_wiz.py
@@ -8,6 +8,7 @@ from StringIO import StringIO
 from openerp.tools import ustr
 
 import unicodecsv
+import re
 from openerp import api, fields, models, _
 from openerp.exceptions import Warning as UserError
 
@@ -20,6 +21,19 @@ class CamposImportGrpDueWiz(models.TransientModel):
     filename = fields.Char('Filename')
 
 
+    @api.model
+    def _prepare_csv_encoding(self):
+        '''This method is designed to be inherited'''
+        return 'latin1'
+    
+    @api.model
+    def _csv_convert_amount(self, amount_str):
+        '''This method is designed to be inherited'''
+        valstr = re.sub(r'[^\d,.-]', '', amount_str)
+        valstrdot = valstr.replace('.', '')
+        valstrdot = valstrdot.replace(',', '.')
+        return float(valstrdot)
+    
     @api.multi
     def import_file(self):
         """Process the file chosen in the wizard, create bank statement(s) and
@@ -29,11 +43,18 @@ class CamposImportGrpDueWiz(models.TransientModel):
         f = StringIO()
         f.write(data_file.lstrip())
         f.seek(0)
+        n = 0
         try:
             for line in unicodecsv.DictReader(
-                    f, encoding=self._prepare_csv_encoding(), delimiter=';', fieldnames=['date', 'ref', 'partner', 'amount']):
-                pass
+                    f, encoding=self._prepare_csv_encoding(), delimiter=';'):
+                refnr = line['Nummer']
+                n += 1
+                grp = self.env['event.registration'].search([('partner_id.ref','=', refnr)])
+                if grp:
+                    grp[0].nav_due_amount = self._csv_convert_amount(line['Saldo (RV)'])
+                    if line['Valutakode'] == 'EUR':
+                        grp[0].nav_due_amount_eur = self._csv_convert_amount(line['Saldo'])
         except Exception as e:
-            raise UserError(_('File parse error:\n%s') % ustr(e))
+            raise UserError(_('File parse error:\n%s\nLine: %d') % (ustr(e), n))
 
     
