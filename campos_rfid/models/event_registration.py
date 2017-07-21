3134c8ce837b614a3384491dccb0376086d361fb
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2017-07-21 14:11:30 +0200

    Catering tickets from group

--- campos_rfid/models/event_registration.py
+++ campos_rfid/models/event_registration.py
@@ -4,6 +4,8 @@
 
 from openerp import api, fields, models, _
 
+import logging
+_logger = logging.getLogger(__name__)
 
 class EventRegistration(models.Model):
 
@@ -11,15 +13,19 @@ class EventRegistration(models.Model):
 
     catering_note = fields.Text('Catering Note')
     catering_ticket_ids = fields.One2many('campos.cat.ticket', 'registration_id', 'Catering Tickets')
-    
+
     @api.multi
     def generate_catering_tickets(self):
+        _logger.info('CAT TICKETS')
         for reg in self:
             if not reg.catering_ticket_ids:
-                for day in self.env['event.day'].search([('event_date', '>=', reg.tocampdate),('event_date', '>=', reg.fromcampdate)]):
-                    cat_inst = self.env['campos.cat.inst'].search([('subcamp_id', '=', reg.subcamp_id),('date','=', day.event_date)])
-                    tck = self.env['campos.cat.ticket'].create({'regisstration_id': reg.id,
+                for day in self.env['event.day'].search([('event_date', '>=', reg.tocampdate),('event_date', '<=', reg.fromcampdate)]):
+                    cat_inst = self.env['campos.cat.inst'].search([('subcamp_id', '=', reg.subcamp_id.id),('date','=', day.event_date)])
+                    _logger.info('DAY: %s INST %s', day.event_date, cat_inst)
+                    tck = self.env['campos.cat.ticket'].create({'registration_id': reg.id,
                                                                 'cat_inst_id': cat_inst.id})
+                    _logger.info('GEN: %s', tck)
                     meatlist = self.env['event.registration.meatlist'].search([('registration_id', '=', reg.id),('event_date','=', day.event_date)])
                     if meatlist:
+                        _logger.info('MEAT: %s', meatlist)
                         meatlist.write({'ticket_id': tck.id})
